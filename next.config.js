64dfa79efa3355c88e954dbcc0895a5b45a95a16
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-08-16 01:00:13 +0300

    delete config next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const {PHASE_DEVELOPMENT_SERVER} = require('next/constants')
-
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {env: {
-      NEXTAUTH_URL : 'http://localhost:3000'
-    }}
-  }
-  return {env: {NEXTAUTH_URL : 'https://next-auth-mu-ten.vercel.app'}}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
